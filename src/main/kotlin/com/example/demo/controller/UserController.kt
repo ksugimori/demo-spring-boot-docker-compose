e1c88f9750a6f03e579ae86c517831f3a498df9e
ksugimori <ksugimori.private@gmail.com>
2025-01-02 08:58:45 +0900

    import * を使わないように変更

--- src/main/kotlin/com/example/demo/controller/UserController.kt
+++ src/main/kotlin/com/example/demo/controller/UserController.kt
@@ -2,11 +2,17 @@ package com.example.demo.controller
 
 import com.example.demo.dto.User
 import com.example.demo.mapper.UserMapper
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
-
 
 @RestController
 @RequestMapping("/users")
--- src/main/kotlin/com/example/demo/mapper/UserMapper.kt
+++ src/main/kotlin/com/example/demo/mapper/UserMapper.kt
@@ -1,7 +1,13 @@
 package com.example.demo.mapper
 
 import com.example.demo.dto.User
-import org.apache.ibatis.annotations.*
+import org.apache.ibatis.annotations.Delete
+import org.apache.ibatis.annotations.Insert
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Options
+import org.apache.ibatis.annotations.Param
+import org.apache.ibatis.annotations.Select
+import org.apache.ibatis.annotations.Update
 
 @Mapper
 interface UserMapper {
